e8cf7df1e110f83bb0a66abdfacbf9735039c0ce
sunicyosen <sunicyosen@outlook.com>
2020-03-27 12:29:56 +0800

    Add h_theta and gradient

--- linear-regression/functions.py
+++ linear-regression/functions.py
@@ -1,7 +1,10 @@
 # /usr/bin/python3
 
 def h_theta(THETA, X):
-    result = 0
+    result = X * THETA
     return result
 
-def gradient(THETA, X)
+def gradient(THETA, X, Y):
+    H_THETA        = h_theta(THETA, X)
+    gradient_value = X.T * (Y - H_THETA)
+    return gradient_value
