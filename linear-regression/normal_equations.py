6352df71af8f517e017082160544fbf36a0432d6
sunicyosen <sunicyosen@outlook.com>
2020-03-27 12:32:15 +0800

    Add normal equations direct method

---
+++ linear-regression/normal_equations.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from functions import gradient
+from cost_fuction import cost_function
+
+def normal_quations(data_matrix, label_matrix):
+    cost_vector   = []
+
+    theta            = (data_matrix.T * data_matrix).I * data_matrix.T * label_matrix
+    
+    cost_j           = cost_function(theta, data_matrix, label_matrix)
+    cost_vector.append(cost_j[0,0])
+
+    return theta, cost_vector
