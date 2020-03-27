eba4d1efa488c2b42a9138058eff5d78be094c4d
sunicyosen <sunicyosen@outlook.com>
2020-03-27 12:30:17 +0800

    Add Gradient descent method

---
+++ linear-regression/gradient_descent.py
@@ -0,0 +1,26 @@
+#
+# Gradient Descent
+#
+
+import numpy as np
+
+from functions import gradient
+from cost_fuction import cost_function
+
+def gradient_descent(data_matrix, label_matrix, alpha, max_iter_numbers):
+    step_alpha    = alpha
+    epoches       = max_iter_numbers
+
+    m,n           = np.shape(data_matrix)
+    theta         = np.zeros((n,1))    # Initial theta
+    cost_vector   = []
+
+    for epoch in range(epoches):
+        cost_j           = cost_function(theta, data_matrix, label_matrix)
+        cost_vector.append(cost_j[0,0])
+        theta            = theta + step_alpha * gradient(theta, data_matrix, label_matrix)
+    
+    cost_j               = cost_function(theta, data_matrix, label_matrix)
+    cost_vector.append(cost_j[0,0])
+
+    return theta, cost_vector
