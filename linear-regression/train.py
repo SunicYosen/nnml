2a92576d119e26ba81f5b44dc681a00320e0bd48
sunicyosen <sunicyosen@outlook.com>
2020-03-28 10:44:49 +0800

    Remove No use import

--- linear-regression/train.py
+++ linear-regression/train.py
@@ -2,13 +2,8 @@
 # Train
 #
 
-import matplotlib.pyplot as plt
-import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 from gradient_descent import gradient_descent
 from normal_equations import normal_quations
-from load_data import load_train_data, load_test_data
-from test import test
 
 def train(data_mat, label_mat, epoches, alpha):
     max_iter_count          = epoches
