a672d76260363a9c8322c37deecf8f9a1b1e160c
sunicyosen <sunicyosen@outlook.com>
2020-04-10 00:20:02 +0800

    add load data functio

---
+++ clustering/load_data.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+#
+# Load data function
+# file_name: the data file name
+# 
+
+def load_data(file_name):
+
+    data_array  = []
+
+    file_point = open(file_name)
+
+    for line in file_point.readlines():
+        line_array = line.strip().split(' ')
+        data_array.append([float(line_array[0]), float(line_array[1])]) 
+
+    return data_array
+
+if __name__ == "__main__":
+    data_array = load_data("data.txt")
+    print(data_array)
