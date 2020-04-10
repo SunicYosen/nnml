78511f3aa54fcb8b4b53199ce7ca2dfb97f09764
sunicyosen <sunicyosen@outlook.com>
2020-04-10 11:27:41 +0800

    Add Distance Calculation

---
+++ clustering/distance.py
@@ -0,0 +1,29 @@
+
+
+def _euclidean_distance(point_a, point_b):
+    '''
+    Function: return the euclidean distance of two points without weights
+    Input: Two points
+    '''
+    distance = np.math.sqrt(sum(np.power(point_a - point_b, 2)))
+
+    return distance
+
+def _manhattan_distance(point_a, point_b):
+    '''
+    Function: return the manhattan distance of two points without weights
+    Input: Two points
+    '''
+    distance = sum(np.abs(point_a - point_b))
+
+    return distance
+
+
+def _minkowski_distance(point_a, point_b, q):
+    '''
+    Function: return the minkowski distance of two points without weights
+    Input: Two points and q
+    '''
+    distance = np.math.power(sum(np.power(np.abs(point_a - point_b), q), float(1.0/q)))
+
+    return distance
